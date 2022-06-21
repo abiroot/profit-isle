c76f0ce8189ae1398c0f6f6d9c0255533b21bbea
Ayman Abi Aoun <ayman@abiroot.com>
2022-06-21 23:22:58 +0300

    Fixed Testimonial

--- home/app.js
+++ home/app.js
@@ -27,43 +27,56 @@ gsap.from(".button-animation", {
 
 // });
 
+// A $( document ).ready() block.
+$(document).ready(function() {
+    $(".section-testimonials [data-section]").on("click", function(el) {
 
-const targetDiv1 = document.getElementById("container1");
-const targetDiv2 = document.getElementById("container2");
-const targetDiv3 = document.getElementById("container3");
-const text1 = document.getElementById("text1");
-const text2 = document.getElementById("text2");
-const text3 = document.getElementById("text3");
+        let sectionID = $(this).attr('data-section');
+        $(".section-testimonials .col-md-2 [data-section]").each(function() {
+            $(this).addClass('opacity-75');
+        });
+        $(this).removeClass("opacity-75");
 
-targetDiv1.onclick = function() {
-
-    text1.style.display = "block";
-    text2.style.display = "none";
-    text3.style.display = "none";
-    targetDiv1.style.opacity = "1";
-    targetDiv2.style.opacity = "0.7";
-    targetDiv3.style.opacity = "0.7";
-
-
-}
-
-targetDiv2.onclick = function() {
-    text1.style.display = "none";
-    text2.style.display = "block";
-    text3.style.display = "none";
-    targetDiv1.style.opacity = "0.7";
-    targetDiv3.style.opacity = "0.7";
-    targetDiv2.style.opacity = "1";
+        $(".section-testimonials .testimonial").each(function(e) {
+            $(this).removeClass("active-section");
+        })
+        $(".section-testimonials .testimonial[data-section=" + sectionID + "]").addClass("active-section");
+    })
+});
 
 
-}
-targetDiv3.onclick = function() {
-    text1.style.display = "none";
-    text2.style.display = "none";
-    text3.style.display = "block";
-    targetDiv2.style.opacity = "0.7";
-    targetDiv1.style.opacity = "0.7";
-    targetDiv3.style.opacity = "1";
+// const targetDiv1 = document.getElementById("container1");
+// const targetDiv2 = document.getElementById("container2");
+// const targetDiv3 = document.getElementById("container3");
+// const text1 = document.getElementById("text1");
+// const text2 = document.getElementById("text2");
+// const text3 = document.getElementById("text3");
+
+// targetDiv1.onclick = function() {
+//     text1.style.display = "block";
+//     text2.style.display = "none";
+//     text3.style.display = "none";
+//     targetDiv1.style.opacity = "1";
+//     targetDiv2.style.opacity = "0.7";
+//     targetDiv3.style.opacity = "0.7";
+// }
+
+// targetDiv2.onclick = function() {
+//     text1.style.display = "none";
+//     text2.style.display = "block";
+//     text3.style.display = "none";
+//     targetDiv1.style.opacity = "0.7";
+//     targetDiv3.style.opacity = "0.7";
+//     targetDiv2.style.opacity = "1";
 
 
-}
+// }
+// targetDiv3.onclick = function() {
+//     text1.style.display = "none";
+//     text2.style.display = "none";
+//     text3.style.display = "block";
+//     targetDiv2.style.opacity = "0.7";
+//     targetDiv1.style.opacity = "0.7";
+//     targetDiv3.style.opacity = "1";
+// }   targetDiv3.style.opacity = "1";
+// }
